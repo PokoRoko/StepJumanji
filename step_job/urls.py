77317933721c57a6a3c70e8424762b68ef5ff1d9
PokoRoko <peuc386@gmail.com>
2021-06-01 01:02:03 +0300

    Up url

--- step_job/urls.py
+++ step_job/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 
-from .views import (companies_view, custom_handler400, custom_handler403,
-                    custom_handler404, custom_handler500, index_view,
-                    specialization_view, vacancies_view, vacancy_view)
+from .views import  custom_handler400, custom_handler403, custom_handler404, custom_handler500, IndexView,DetailVacancyView,DetailCompanyViews,ListVacanciesView,ListSpecializationView
 
 handler400 = custom_handler400
 handler403 = custom_handler403
@@ -10,9 +8,9 @@ handler404 = custom_handler404
 handler500 = custom_handler500
 
 urlpatterns = [
-    path('', index_view, name="main"),
-    path('vacancies/', vacancies_view, name="vacancies"),
-    path('vacancies/cat/<str:name_spec>/', specialization_view, name="specialization"),
-    path('companies/<int:id_company>/', companies_view, name="companies"),
-    path('vacancies/<int:id_vacancy>/', vacancy_view, name="vacancy"),
+    path('', IndexView, name="main"),
+    path('vacancies/', ListVacanciesView, name="vacancies"),
+    path('vacancies/cat/<str:name_spec>/', ListSpecializationView, name="specialization"),
+    path('companies/<int:id_company>/', DetailCompanyViews, name="companies"),
+    path('vacancies/<int:id_vacancy>/', DetailVacancyView, name="vacancy"),
 ]
