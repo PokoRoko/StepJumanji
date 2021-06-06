21b76cd7cce18611e05243ca1a00511c7de82ff8
PokoRoko <peuc386@gmail.com>
2021-06-06 14:57:32 +0300

    Add all models and choices

--- mock_data.py
+++ mock_data.py
@@ -78,38 +78,3 @@ specialties = [
 
 ]
 
-""" Статусы в формате Enum """
-
-#
-#
-# class EducationChoices(Enum):
-#     missing = 'Отсутствует'
-#     secondary = 'Среднее'
-#     vocational = 'Средне-специальное'
-#     incomplete_higher = 'Неполное высшее'
-#     higher = 'Высшее'
-#
-#
-# class GradeChoices(Enum):
-#     intern = 'intern'
-#     junior = 'junior'
-#     middle = 'middle'
-#     senior = 'senior'
-#     lead = 'lead'
-#
-#
-# class SpecialtyChoices(Enum):
-#     frontend = 'Фронтенд'
-#     backend = 'Бэкенд'
-#     gamedev = 'Геймдев'
-#     devops = 'Девопс'
-#     design = 'Дизайн'
-#     products = 'Продукты'
-#     management = 'Менеджмент'
-#     testing = 'Тестирование'
-#
-#
-# class WorkStatusChoices(Enum):
-#     not_in_search = 'Не ищу работу'
-#     consideration = 'Рассматриваю предложения'
-#     in_search = 'Ищу работу'
--- step_job/models.py
+++ step_job/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-from StepikJumanji.settings import BASE_DIR
+from StepikJumanji.settings import MEDIA_COMPANY_IMAGE_DIR, MEDIA_SPECIALITY_IMAGE_DIR
 
 
 class Company(models.Model):
     name = models.CharField(max_length=64)
     location = models.CharField(max_length=64)
-    logo = models.URLField(default='https://place-hold.it/100x60')
+    logo = models.ImageField(default='https://place-hold.it/100x60', upload_to=MEDIA_COMPANY_IMAGE_DIR)
     description = models.CharField(max_length=64)
-    employee_count = models.IntegerField()
+    employee_count = models.PositiveIntegerField()
     owner = models.OneToOneField(User, on_delete=models.CASCADE, related_name="Companies")
 
     class Meta:
@@ -23,7 +23,7 @@ class Company(models.Model):
 class Specialty(models.Model):
     code = models.CharField(max_length=64, unique=True)
     title = models.CharField(max_length=64)
-    picture = models.URLField(default='https://place-hold.it/100x60')
+    picture = models.ImageField(default='https://place-hold.it/100x60', upload_to=MEDIA_SPECIALITY_IMAGE_DIR)
 
     class Meta:
         verbose_name = "Специализация"
@@ -39,8 +39,8 @@ class Vacancy(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name="vacancies")
     skills = models.CharField(max_length=64)
     description = models.CharField(max_length=64)
-    salary_min = models.IntegerField()
-    salary_max = models.IntegerField()
+    salary_min = models.PositiveIntegerField()
+    salary_max = models.PositiveIntegerField()
     published_at = models.DateTimeField(auto_now_add=True, blank=True)
 
     class Meta:
@@ -64,5 +64,47 @@ class Application(models.Model):
 
     def __str__(self):
         return f"Application {self.user} to {self.vacancy}"
+
+
+class Resume(models.Model):
+    class EducationChoices(models.TextChoices):
+        missing = 'Отсутствует'
+        secondary = 'Среднее'
+        vocational = 'Средне-специальное'
+        incomplete_higher = 'Неполное высшее'
+        higher = 'Высшее'
+
+    class GradeChoices(models.TextChoices):
+        intern = 'intern'
+        junior = 'junior'
+        middle = 'middle'
+        senior = 'senior'
+        lead = 'lead'
+
+    class SpecialtyChoices(models.TextChoices):
+        frontend = 'Фронтенд'
+        backend = 'Бэкенд'
+        gamedev = 'Геймдев'
+        devops = 'Девопс'
+        design = 'Дизайн'
+        products = 'Продукты'
+        management = 'Менеджмент'
+        testing = 'Тестирование'
+
+    class WorkStatusChoices(models.TextChoices):
+        not_in_search = 'Не ищу работу'
+        consideration = 'Рассматриваю предложения'
+        in_search = 'Ищу работу'
+
+    user = models.OneToOneField(User, related_name="Resumes", on_delete=models.CASCADE)
+    name = models.CharField(max_length=64)
+    surname = models.CharField(max_length=64)
+    status = models.CharField(choices=WorkStatusChoices, max_length=64)
+    salary = models.PositiveIntegerField()
+    specialty = models.CharField(choices=SpecialtyChoices, max_length=512)
+    grade = models.CharField(choices=GradeChoices, max_length=64)
+    education = models.CharField(choices=EducationChoices, max_length=64)
+    experience = models.CharField(max_length=512)
+    portfolio = models.CharField(max_length=1028)
 
 
